3e5794177411e15aa53c617e249c2c7ec24f84c8
landrushka <a.livintsev@gmail.com>
2023-11-01 14:17:49 +0100

    iter8: first commit

--- internal/utils/gzip_test.go
+++ internal/utils/gzip_test.go
@@ -4,55 +4,8 @@ import (
 	"compress/gzip"
 	"io"
 	"net/http"
-	"reflect"
 	"testing"
 )
-
-func TestNewCompressReader(t *testing.T) {
-	type args struct {
-		r io.ReadCloser
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    *compressReader
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := NewCompressReader(tt.args.r)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("NewCompressReader() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewCompressReader() got = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestNewCompressWriter(t *testing.T) {
-	type args struct {
-		w http.ResponseWriter
-	}
-	tests := []struct {
-		name string
-		args args
-		want *compressWriter
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := NewCompressWriter(tt.args.w); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewCompressWriter() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
 
 func Test_compressReader_Close(t *testing.T) {
 	type fields struct {
@@ -134,31 +87,6 @@ func Test_compressWriter_Close(t *testing.T) {
 			}
 			if err := c.Close(); (err != nil) != tt.wantErr {
 				t.Errorf("Close() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
-func Test_compressWriter_Header(t *testing.T) {
-	type fields struct {
-		w  http.ResponseWriter
-		zw *gzip.Writer
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   http.Header
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := &compressWriter{
-				w:  tt.fields.w,
-				zw: tt.fields.zw,
-			}
-			if got := c.Header(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Header() = %v, want %v", got, tt.want)
 			}
 		})
 	}
