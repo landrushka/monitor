a4d24cefd3cee57a45b6dd003555c191e7a89a06
landrushka <a.livintsev@gmail.com>
2023-11-03 19:49:39 +0100

    iter8: add logging

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -28,21 +28,27 @@ func GzipMiddleware(h http.Handler) http.Handler {
 		ow := w
 
 		// проверяем, что клиент умеет получать от сервера сжатые данные в формате gzip
-		acceptEncoding := r.Header.Get("Accept-Encoding")
-		supportsGzip := strings.Contains(acceptEncoding, "gzip")
-		if supportsGzip {
+		//acceptEncoding := r.Header.Get("Accept-Encoding")
+		//supportsGzip := strings.Contains(acceptEncoding, "gzip")
+		//if supportsGzip {
+		//	// оборачиваем оригинальный http.ResponseWriter новым с поддержкой сжатия
+		//	cw := utils.NewCompressWriter(w)
+		//	// меняем оригинальный http.ResponseWriter на новый
+		//	ow = cw
+		//	// не забываем отправить клиенту все сжатые данные после завершения middleware
+		//	defer cw.Close()
+		//}
+
+		// проверяем, что клиент отправил серверу сжатые данные в формате gzip
+		contentEncoding := r.Header.Get("Content-Encoding")
+		sendsGzip := strings.Contains(contentEncoding, "gzip")
+		if sendsGzip {
 			// оборачиваем оригинальный http.ResponseWriter новым с поддержкой сжатия
 			cw := utils.NewCompressWriter(w)
 			// меняем оригинальный http.ResponseWriter на новый
 			ow = cw
 			// не забываем отправить клиенту все сжатые данные после завершения middleware
 			defer cw.Close()
-		}
-
-		// проверяем, что клиент отправил серверу сжатые данные в формате gzip
-		contentEncoding := r.Header.Get("Content-Encoding")
-		sendsGzip := strings.Contains(contentEncoding, "gzip")
-		if sendsGzip {
 			// оборачиваем тело запроса в io.Reader с поддержкой декомпрессии
 			cr, err := utils.NewCompressReader(r.Body)
 			w.Header().Set("Content-Encoding", "gzip")
